59d6cd95fb99ea5f8e5c0f2913b70b806bcd6b56
Sr-L <luisanfincias@gmail.com>
2021-05-07 17:26:11 +0200

    first

---
+++ info.js
@@ -0,0 +1,27 @@
+/* const http = require("http");
+
+const host ='localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("My first server!");
+};
+
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+
+ */
+
+
+const axios = require('axios');
+
+axios({
+    method: 'get',
+    url: 'https://dotnetsafer.com/blog/wp-json/wp/v2/posts?_embed'
+  }).then(res=>{console.log(res.data)}
+
+  )
