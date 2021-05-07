611b1aa0728f68dd2767e28ee7d08fd26744e2f4
Sr-L <luisanfincias@gmail.com>
2021-05-07 17:39:57 +0200

    cads

--- info.js
+++ info.js
@@ -19,16 +19,11 @@ server.listen(port, host, () => {
 
 const axios = require('axios');
 
-
-window.onload(
-
-    axios({
+axios({
         method: 'get',
         url: 'https://dotnetsafer.com/blog/wp-json/wp/v2/posts?_embed'
       }).then(res=>{console.log(res.data)}
     
       )
 
-
-
-)
+console.log("js cargado")
